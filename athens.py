4049df69fcfdcf4509c6fdbc634743614554df41
meteogus <loutaskon@gmail.com>
2025-07-12 23:44:17 +0300

    Update athens.py

--- athens.py
+++ athens.py
@@ -405,6 +405,8 @@ for tick, label in zip(ticks_00z, labels_00z):
 
 
 
+run_hour = latest_run_time.strftime("%H")
+filename = f"athens{run_hour}.png"
 plt.subplots_adjust(hspace=0.05)
-plt.savefig("athens_meteogram.png", dpi=96, bbox_inches='tight', pad_inches=0)
+plt.savefig(filename, dpi=96, bbox_inches='tight', pad_inches=0)
 plt.close(fig)
