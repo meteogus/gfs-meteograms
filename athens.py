69d376d2e2b6dd6349e7a74023150291ff6e6977
meteogus <loutaskon@gmail.com>
2025-07-14 01:33:57 +0300

    Update athens.py

--- athens.py
+++ athens.py
@@ -25,8 +25,8 @@ if now_utc.hour < gfs_run_hours[0]:
 print(f"Latest GFS run: {latest_run_time:%Y-%m-%d %HZ}")
 
 # Location
-latitude = 37.97
-longitude = 23.72
+latitude = 38.00
+longitude = 23.75
 
 # API call parameters
 url = "https://api.open-meteo.com/v1/forecast"
