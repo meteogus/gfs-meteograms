fe00db45e5c4fc828b085a1f40d75692db306927
meteogus <loutaskon@gmail.com>
2025-08-05 18:46:45 +0300

    Update ikaria.py

--- ikaria.py
+++ ikaria.py
@@ -32,8 +32,8 @@ if now_utc.hour < gfs_run_hours[0]:
 print(f"Latest GFS run: {latest_run_time:%Y-%m-%d %HZ}")
 
 # Location
-latitude = 54
-longitude = 0
+latitude = 37.63
+longitude = 26.22
 
 
 # API call parameters
@@ -1087,5 +1087,6 @@ filename = f"ikaria{run_hour}.png"
 plt.subplots_adjust(hspace=0.05)
 plt.savefig(filename, dpi=96, bbox_inches='tight', pad_inches=0)
 plt.close(fig)
+
 
 
