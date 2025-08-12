2532c974e9b251f10feb44d6e0383f6532f7c792
meteogus <loutaskon@gmail.com>
2025-08-12 18:27:23 +0300

    Update athens.py

--- athens.py
+++ athens.py
@@ -94,7 +94,7 @@ params = {
     ]),
     "forecast_days": 6,
     "timezone": "UTC",
-    "models": "gfs_seamless"
+    "models": "gfs_global"
 }
 
 max_total_wait = 900
@@ -1110,6 +1110,7 @@ filename = f"athens{run_hour}.png"
 plt.subplots_adjust(hspace=0.05)
 plt.savefig(filename, dpi=96, bbox_inches='tight', pad_inches=0)
 plt.close(fig)
+
 
 
 
